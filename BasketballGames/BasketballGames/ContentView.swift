3837efdb16451be81b3f143af4ffe21c614de913
Maia Manaligod <maia.manaligod@gmail.com>
2025-03-02 21:39:32 -0500

    Implement Basketball App

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,17 +7,74 @@
 
 import SwiftUI
 
+
+struct Result: Codable {
+    var team: String
+    var isHomeGame: Bool
+    var id: Int
+    var date: String
+    var score: Score
+    var opponent: String
+}
+
+struct Score: Codable {
+    var opponent: Int
+    var unc: Int
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List (results, id: \.id) {item in
+                HStack{
+                    VStack (alignment: .leading){
+                        Text("\(item.team) vs. \(item.opponent)")
+                        Text("\(item.date)")
+                            .font(.caption)
+                            .foregroundStyle(.secondary)
+                    }
+                    Spacer()
+                    
+                    VStack (alignment: .trailing){
+                        Text("\(item.score.unc) - \(item.score.opponent)")
+                        Text(item.isHomeGame ? "Home" : "Away")
+                            .font(.caption)
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    
+                }
+            }.navigationTitle("UNC Basketball")
         }
-        .padding()
+        .task{
+            await loadData()
+        }
+    }
+    
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode([Result].self, from: data){
+
+                results = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
+                
     }
 }
+
+
+
+
 
 #Preview {
     ContentView()
